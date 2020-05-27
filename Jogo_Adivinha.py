72198520bd548f2859245bb8b2ed28562f55c49a
Guilherme Dias Cardoso <guilhermedicasil@gmail.com>
2020-05-27 18:01:21 -0300

    Função erros.

--- Jogo_Adivinha.py
+++ Jogo_Adivinha.py
@@ -4,13 +4,20 @@ os.system("clear")
 num=random.randrange(1,10)
 erro=0
 while 1:
+    if erro==3:
+        print("Você perdeu!!")
+        print(f"O numero era {num}")
+        break
     escolhido=int(input("Digite um número escolhido: "))
+    os.system("clear")
     if escolhido>num:
         print("O número é menor!!!")
         erro+=1
+        print(f"Erros {erro}")
     elif escolhido<num:
         print("O numero é maior!!!")
         erro+=1
+        print(f"Erros {erro}")
     elif escolhido==num:
         print("Parabéns, você acertou:")
         break
